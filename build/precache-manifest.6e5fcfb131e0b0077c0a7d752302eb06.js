4498d0674a2b3edeb5ee46c957369427fc39c191
dennisx <denizponce16@gmail.com>
2020-08-09 20:36:34 -0500

    deploy

---
+++ build/precache-manifest.6e5fcfb131e0b0077c0a7d752302eb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1fe1ff8900b391b3608902af7a443e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a84d1d623395f5e72c0",
+    "url": "/static/css/main.1b678bd7.chunk.css"
+  },
+  {
+    "revision": "4bc9ca5d15098f3bf96d",
+    "url": "/static/js/2.04641555.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.04641555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a84d1d623395f5e72c0",
+    "url": "/static/js/main.cdb5498a.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
