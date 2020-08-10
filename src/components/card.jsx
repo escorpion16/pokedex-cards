d3c07c037b76c17ff2f7d0f08f75ea04f6e5fd05
dennisx <denizponce16@gmail.com>
2020-08-09 20:10:52 -0500

    Se instalo material-ui y se implemento barra de progreso

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 export default function Card(props){
 
     return (
-        <div className="wrap-card">
-            <div className="card">
-                <div className="front-card">
+        <div className="wrap-card"  >
+            <div className="card"  id="testbutton">
+                <div className="front-card" >
                     <img src={props.pokemon.sprite} alt="pokemon"/>
                 </div>
                 <div className="back-card">
                     <h2>{props.pokemon.name.english}</h2>
-                    <div>
-                        <ul>
+                    <div className="list-powers">
+                        <div className="linearProgress">
+                            <h3>HP</h3>
+                            <LinearProgress variant="determinate" value={props.powerPokemon.HP} />
+                        </div>
+                        <div className="linearProgress">
+                            <h3>Attack</h3>
+                            <LinearProgress variant="determinate" value={props.powerPokemon.Attack} />
+                        </div>
+                        <div className="linearProgress">
+                            <h3>Defense</h3>
+                            <LinearProgress variant="determinate" value={props.powerPokemon.Defense} />
+                        </div>
+                        <div className="linearProgress">
+                            <h3>Speed</h3>
+                            <LinearProgress variant="determinate" value={props.powerPokemon.Speed} />
+                        </div>
+                        {/* <ul>
                             <li>HP: {props.powerPokemon.HP}</li>
                             <li>Attack: {props.powerPokemon.Attack}</li>
                             <li>Defense: {props.powerPokemon.Defense}</li>
                             <li>Speed: {props.powerPokemon.Speed}</li>
-                        </ul>
+                        </ul> */}
                     </div>
-                    <div >
+                    <div className="btn-power">
                         {props.pokemon.type.map(type => {
                             return <span className={`types ${type}`}>{type}</span> 
                         })}
